8af21f22c572dd3aa7b5fecbe77081c49c0545e8
Raushanray <raushankuray1998@gmail.com>
2023-10-04 17:18:58 +0530

    first commit

---
+++ src/main/java/com/lcwr/SBMapperValidation/SbMapperValidationApplication.java
@@ -0,0 +1,13 @@
+package com.lcwr.SBMapperValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMapperValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMapperValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcwr/SBMapperValidation/SbMapperValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwr.SBMapperValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMapperValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
